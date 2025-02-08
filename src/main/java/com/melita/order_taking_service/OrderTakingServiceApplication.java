bdd2fa45d442b06255dfdf7172122b600558de0e
Vincenzo Sciancalepore <sciancalepore.vincenzo@gmail.com>
2025-02-08 16:56:15 +0100

    First Commit

---
+++ src/main/java/com/melita/order_taking_service/OrderTakingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.melita.order_taking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderTakingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderTakingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melita/order_taking_service/OrderTakingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melita.order_taking_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderTakingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
