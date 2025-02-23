1977b729d2f626f6500efa1a3dc4cd1f136fd482
Vincenzo Sciancalepore <sciancalepore.vincenzo@gmail.com>
2025-02-23 12:50:18 +0100

    organized application.yml and deployment for dev evn. deleted postgresql and jpa

--- src/main/java/com/melita/ots/OrderTakingServiceApplication.java
+++ src/main/java/com/melita/ots/OrderTakingServiceApplication.java
@@ -1,13 +1,12 @@
 package com.melita.ots;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class OrderTakingServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderTakingServiceApplication.class, args);
+		ConfigurableEnvironment environment = SpringApplication.run(OrderTakingServiceApplication.class, args).getEnvironment();
 	}
-
 }
